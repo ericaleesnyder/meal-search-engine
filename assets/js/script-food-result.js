e513c7f3d4f0a9fb0348cc35d1197b138e4124c2
Erica Snyder <ericaleesnyder@gmail.com>
2021-10-06 17:19:50 -0700

    added button functionality to add a food recipe to favorites

--- assets/js/script-food-result.js
+++ assets/js/script-food-result.js
@@ -42,3 +42,85 @@
 //{"risotto", "https://......"}]
 
 //RAGHAD - write the JS to create cards from the array
+
+// var favoritedRecipes = [
+//   {name: "chicken parmesan", url: "https://cafedelites.com/crispy-chicken-parmesan/"},
+//   {name: "eggplant parmesan", url: "https://www.foodnetwork.com/recipes/food-network-kitchen/eggplant-parmesan-recipe-2008982"},
+//   {name: "margarita", url: "https://www.liquor.com/recipes/margarita/"},
+// ]
+
+// function displayFoodResults(results) {
+//   // console.log(results); 
+//   // may need this to checkout the full data response 
+//   for (var i = 0; i < results.length; i++) {
+//   // the results should return an array of 20 items
+//     var result = results[i];
+    // var name = result.ingredients.lable; RESULTS.INGREDIENTS.LABLE WILL BE THE TEXT CONTENT FOR CARD-TITLE
+//     var cuisineType = result.cuisineType[0];
+//     var mealType = result.mealType[0];
+//     var url = result.url;
+
+//   //write code to display the variables
+    
+//   }
+// }
+
+
+
+var userFavs = document.getElementById('user-favorites')
+var favoritedRecipes = []
+
+var cardDisplay = document.getElementById('card-display');
+var favBtn = document.getElementsByClassName('favorite-button');
+
+function renderFavs () {
+  userFavs.innerHTML="";
+  
+  for (var i =0; i < favoritedRecipes.length; i++) {
+    var recipeAdd = favoritedRecipes[i].name;
+    
+    var favoritedBtn = document.createElement('a');
+    
+    favoritedBtn.textContent = recipeAdd
+    favoritedBtn.setAttribute('data-index', i);
+    favoritedBtn.setAttribute('class', 'waves-effect waves-light btn button')
+    
+  
+    userFavs.appendChild(favoritedBtn);
+  
+  }
+};
+
+function init () {
+  var storedFavs = JSON.parse(localStorage.getItem('fav food'));
+  if (storedFavs !== null){
+    favoritedRecipes = storedFavs
+  }
+  renderFavs();
+}
+
+function storeFav () {
+  localStorage.setItem('fav food', JSON.stringify(favoritedRecipes));
+}
+
+
+cardDisplay.addEventListener('click', function (event) {
+  event.preventDefault();
+  
+  var element = event.target;
+ 
+  if (element.matches('i')) {
+    var recipeName = element.parentElement.parentElement.parentElement.firstChild.nextSibling.textContent;
+    console.log(recipeName);
+    
+    
+    favoritedRecipes.push({name: recipeName, url:"url"});
+    
+      
+    renderFavs();
+    storeFav();
+  }
+
+})
+
+init();
