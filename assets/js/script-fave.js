f0349ebf9c47544991f955a857d0cdb4a5488427
Raghad Malallah <raghadmalallah@gmail.com>
2021-10-07 13:30:53 -0700

    final changes for the html, header and footer are ready, card works and tyled and linked

--- assets/js/script-fave.js
+++ assets/js/script-fave.js
@@ -1,22 +1,32 @@
 //RAGHAD - write the JS to create cards from the array
 
 var favoritedRecipes = [];
-var cardDisplay = document.getElementById("card-tabs");
 
-// let storeFav = JSON.parse(localStorage.getItem("storfav")) || []; 
-console.log(storeFav)
-var storeFav = []
-function displayFavCards () {
-  //     localStorage.setItem('fav food', JSON.stringify(favoritedRecipes));
-  // for(let i =0; i storeFav.length; i++){
-  //     let li = document.createElement("li")
-  //     li.textContent = storeFav[i]
-  //     FileList.appendchild(li)
-  // favoritedBtn.setAttribute('data-index', i);
-  // favoritedBtn.setAttribute('class', 'tabs tabs-fixed-width');
-}
 
-displayFavCards();
+let storeFav = JSON.parse(localStorage.getItem("fav food")) || []; 
+console.log(storeFav[0].name)
+
+function displayFavCards (favArr) {
+  for(let i =0; i < favArr.length; i++){
+     document.getElementById("cardDiv").innerHTML+=`
+     <div class="card small">
+        <div class="card-content">
+          <p>
+          ${storeFav[i].name}
+          </p>
+        </div>
+        <div class="card-tabs">
+          <ul class="card-action">
+            <li class="tab"><a href="${storeFav[i].url}">Link</a></li>
+          </ul>
+        </div>
+      </div>
+      `
+}};
+
+displayFavCards(storeFav);
+
+
 
 // when page load
 // check local storage for the storeFav
